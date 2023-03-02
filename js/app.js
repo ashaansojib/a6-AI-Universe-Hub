b4a41c60c4d7f89e213684c0e9024c339aa243f1
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-03-02 21:37:01 +0600

    create modal and connect arrow icon

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// load all data by api link
+const loadAllData = () =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+loadAllData()
