f371e3d1bfe7e11b8eb5d9b802ea96bd2f2f2e2d
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-03-02 23:01:59 +0600

    create details function and generate details link

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ const displayCard = data => {
     // console.log(data.tools)
     const container = document.getElementById("card-container");
     data.tools.forEach(singleTool => {
-        console.log(singleTool)
+        // console.log(singleTool)
         const features = singleTool.features;
         // console.log(features)
         const newContainer = document.createElement("div");
@@ -18,7 +18,7 @@ const displayCard = data => {
         newContainer.innerHTML = `
             <div class="card p-4">
                 <img src="${singleTool.image}" width="full" height="200" class="card-img-top" alt="Image or banner not found!">
-                <div class="card-body">
+                <div class="card-body p-0 pt-2">
                     <h5 class="card-title">Features</h5>
                 <ol>
                     <li>${features[0]}</li>
@@ -33,13 +33,26 @@ const displayCard = data => {
                         <p><i class="fa-solid fa-calendar-days"></i> ${singleTool.published_in}</p>
                     </div>
                     <div>
-                        <a href="#" class="card-link bg-primary text-white py-2 px-3 rounded" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right"></i></a>
+                        <a href="#" onclick="loadDetails('${singleTool.id}')" class="card-link bg-primary text-white py-2 px-3 rounded" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-arrow-right"></i></a>
                     </div>
                 </div>
             </div>
         `;
         container.appendChild(newContainer);
         
-    })
+    });
+}
+
+// create modal details url
+const loadDetails = id =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetails(data.data))
+}
+
+// display details on modal
+const displayDetails = details =>{
+    console.log(details)
 }
 loadAllData()
