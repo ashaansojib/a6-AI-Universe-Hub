8a8d6d0e966ca4621a54d1484ab4e42969b18915
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-03-03 23:59:03 +0600

    projects finalization

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // load all data by api link
-const loadAllData = dataLimit =>{
+const loadAllData = dataLimit => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayCard(data.data, dataLimit))
+        .then(res => res.json())
+        .then(data => displayCard(data.data, dataLimit))
 }
 // display api data with content card
 const displayCard = (data, dataLimit) => {
-        //get features data by loop
+    //get features data by loop
 
     const container = document.getElementById("card-container");
     container.textContent = '';
@@ -15,28 +15,28 @@ const displayCard = (data, dataLimit) => {
     // ----------------------------------------------------------
     const sortData = data => {
         const sortContainer = document.getElementById("sort-area");
-      
+
         // Create a unique date
         const dates = new Set(data.map(item => item.published_in));
-      
+
         // button for all separate date
         dates.forEach(date => {
-          const button = document.createElement("button");
-          button.classList.add("dropdown-item");
-          button.textContent = date;
-      
-          button.addEventListener("click", () => {
-            // find item by date with filtering
-            const filteredData = data.filter(item => item.published_in === date);
-      
-            const cardContainer = document.getElementById("card-container");
-            cardContainer.textContent = "";
-      
-            filteredData.forEach(item => {
-              const features = item.features;
-              const singleCard = document.createElement('div');
-              singleCard.classList.add('col-lg-4', 'col-sm-12', 'col-md-6');
-              singleCard.innerHTML = `
+            const button = document.createElement("button");
+            button.classList.add("dropdown-item");
+            button.textContent = date;
+
+            button.addEventListener("click", () => {
+                // find item by date with filtering
+                const filteredData = data.filter(item => item.published_in === date);
+
+                const cardContainer = document.getElementById("card-container");
+                cardContainer.textContent = "";
+
+                filteredData.forEach(item => {
+                    const features = item.features;
+                    const singleCard = document.createElement('div');
+                    singleCard.classList.add('col-lg-4', 'col-sm-12', 'col-md-6');
+                    singleCard.innerHTML = `
                 <div class="card p-4">
                         <img src="${item.image}" width="full" height="200" class="card-img-top" alt="Image or banner not found!">
                         <div class="card-body p-0 pt-2">
@@ -59,28 +59,28 @@ const displayCard = (data, dataLimit) => {
                         </div>
                     </div>
               `;
-            //   append here child card
-              cardContainer.appendChild(singleCard)
+                    //   append here child card
+                    cardContainer.appendChild(singleCard)
+                });
             });
-          });
-        //   all card add to container
-          sortContainer.appendChild(button);
+            //   all card add to container
+            sortContainer.appendChild(button);
         });
-      };
-      
-      document.getElementById("sort-button").addEventListener("click", () => {
+    };
+
+    document.getElementById("sort-button").addEventListener("click", () => {
         const cardContainer = document.getElementById("card-container");
         cardContainer.innerHTML = "";
-      });
-      
-      sortData(tool);
-      
+    });
+
+    sortData(tool);
+
     // --------------------------------------------------------------------------------------
     // by default 6 card
-    if(dataLimit && tool.length > 6){
+    if (dataLimit && tool.length > 6) {
         tool = tool.slice(0, 6);
     }
-    else{
+    else {
         document.getElementById("show-btn").classList.add('d-none')
     }
     // all card
@@ -88,6 +88,7 @@ const displayCard = (data, dataLimit) => {
         // console.log(features)
         const features = singleTool.features;
         const featureList = features.map(feature => `<li>${feature}</li>`).join('');
+
         const newContainer = document.createElement("div");
         newContainer.classList.add('col-lg-4', 'col-sm-12', 'col-md-6');
         newContainer.innerHTML = `
@@ -117,42 +118,38 @@ const displayCard = (data, dataLimit) => {
 }
 
 // create modal details url
-const loadDetails = id =>{
+const loadDetails = id => {
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
     fetch(url)
-    .then(res => res.json())
-    .then(data => displayDetails(data.data))
+        .then(res => res.json())
+        .then(data => displayDetails(data.data))
 }
 
 // display details on modal
-const displayDetails = details =>{
-    // declare pricing
-    const price = details.pricing;
-    // access features property from objects
+const displayDetails = details => {
+    console.log(details)
+    // acces features
     const features = details.features;
-    // console.log(features)
     const featureList = Object.keys(features).map(key => `<li>${features[key].feature_name}</li>`).join('');
-    // acces integrations property from array
-    const integrations = details.integrations;
-    // console.log(typeof integrations)
-    const integrationsList = Object.keys(integrations ?? {}).map(key => `<li>${integrations[key]}</li>`).join('');
-
+    // acces integrations
+    const integrations = details.integrations?.map(integration => `<li>${integration}</li>`);
+    // access accuracy
+    const accuracy = details.accuracy.score;
+    // access input output
+    const package = details.pricing?.map(basic => `<p>${basic.plan}</p>`);
+    const price = details.pricing?.map(price => `<p>${price.price}</p>`);
+    // console.log(pricing)
     // modal details below
-    const {description, image_link, accuracy, input_output_examples} = details;
+    const { description, image_link } = details;
     const modalContainer = document.getElementById('load-modal')
     modalContainer.innerHTML = `
     <div class="modal-body row">
         <div class="col-lg-6">
             <h4>${description}</h4>
             <div class="row text-center">
-                <div class="col-4 text-success">
-                    <p class="fw-bolder">${price[0].price ? price[0].price : 'Free Bundle'} ${price[0].plan}</p>
-                </div>
-                <div class="col-4 text-warning">
-                    <p class="fw-bolder">${price[1].price ? price[1].price : 'Free Bundle'} ${price[1].plan}</p>
-                </div>
-                <div class="col-4 text-danger">
-                    <p class="fw-bolder">${price[2].price ? price[2].price : 'Free Bundle'} ${price[2].plan}</p>
+                <div class="col text-success">
+                    <div class="d-flex justify-content-around">${package ? package : 'Free Of Cost'}</div>
+                    <div class="d-flex justify-content-around">${price ? price : ''}</div>
                 </div>
             </div>
             <div class="row">
@@ -162,18 +159,18 @@ const displayDetails = details =>{
                 </div>
                 <div class="col-6">
                     <h3>Integrations</h3>
-                    <ol>${integrationsList}</ol>
+                    <ol>${integrations ? integrations : 'Empty Data'}</ol>
                 </div>
             </div>
         </div>
         <div class="col-lg-6">
             <div class="card p-2">
                 <img src="${image_link[0]}" class="card-img-top position-relative" alt="Images not found!!">
-                <h3 class="position-absolute start-50 end-0 p-2 badge rounded-pill bg-danger">${accuracy.score ? `${accuracy.score}% accuracy` : ''}</h3>
+                <h3 class="position-absolute start-50 end-0 p-2 badge rounded-pill bg-danger">${accuracy ? `${accuracy}% accuracy` : ''}</h3>
                 <hr>
                 <div class="card-body text-center">
-                    <h2>${input_output_examples[0].input}</h2>
-                    <p class="card-text">${input_output_examples[0].output ? input_output_examples[0].output : 'No Not Yet, Take a Break'}</p>
+                    <h2>${details?.input_output_examples ? details?.input_output_examples[0]?.input : ''}</h2>
+                    <div class="card-text">${details?.input_output_examples === null ? 'No! Not Yet! Take A Break' : details?.input_output_examples[0]?.output}</div>
                 </div>
             </div>
         </div>
@@ -181,17 +178,17 @@ const displayDetails = details =>{
     `;
 }
 // create spinner function
-const loadSpinner = isLoading =>{
+const loadSpinner = isLoading => {
     const spinnerContainer = document.getElementById("loader-area");
-    if(isLoading){
+    if (isLoading) {
         spinnerContainer.classList.remove('d-none');
     }
-    else{
+    else {
         spinnerContainer.classList.add('d-none')
     }
 }
 
-document.getElementById('showBtn').addEventListener('click', function(){
+document.getElementById('showBtn').addEventListener('click', function () {
     loadAllData();
 });
 loadAllData(6)
